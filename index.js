d7ee7376f0cf64840c64ddef2e607c36b3436fe9
JakobJohnson <madjzj@gmail.com>
2023-07-30 12:40:45 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(strings, event){
+    const message = [];
+    for (let i = 0; i < strings.length; i++){
+        message.push(`Thank you, ${strings[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
+function countDown(int){
+    while(int >= 0){
+        console.log(int--);
+    }
+    
+}
